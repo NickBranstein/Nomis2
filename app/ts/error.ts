863a2f7c6b70c0203ec4b12db9071a4a2087df7d
JasonPMills <JasonPMills@hotmail.com>
2016-08-24 16:38:23 -0400

    updating error control

--- app/ts/error.ts
+++ app/ts/error.ts
@@ -2,15 +2,58 @@ module Engine {
    export class ErrorBox implements IRender, IGetClicked {
        lastTimestamp;
        width: number;
-       
+       outPad: number
+       hPad:number;
+       fPad:number;
         constructor(public x: number, public y: number, public text: string, public height: number, public color: string, private callback?: () => any) {
+            this.outPad = 5;
+            this.hPad = 15;
         }
         
         public render(context: CanvasRenderingContext2D, timestamp): void {    
             // render the sprite
-            context.font = `${this.height}px Arial`;
-            context.fillStyle = this.color;
-            context.fillText(this.text, this.x, this.y);
+            let color1 ='#bbb', color2 ='#aaa', color3 = '#ccc', color4 = '#000000';
+            let btnPadH = 10, btnPadW = 30;
+            let boxPadH = 50, boxPadW = 150;
+            let btnHeight =18;
+            context.fillStyle= color3;
+            context.strokeStyle = color2;
+            let btnYOffset = ((btnPadH/2)+btnHeight);
+            let boxX = this.x-(boxPadW-btnPadW);
+            let boxY = this.y-(this.height+btnHeight+(boxPadH/2));
+            let errorHeight = 12;
+
+            //outer bounding box
+            context.fillRect(boxX,boxY,this.width+boxPadW,this.height+boxPadH);
+            context.strokeRect(
+                this.x-(this.width-btnPadW),
+                boxY,
+                this.width+boxPadW, 
+                this.height+boxPadH
+            );
+            //error message
+            context.strokeStyle = color4;
+            context.fillStyle=color4;
+            context.fillText(this.text, boxX, boxY+errorHeight, this.width);    
+
+            //error button
+            context.font = `${btnHeight}px Arial`;
+            context.strokeStyle = color2;
+            context.fillStyle = color1;
+            context.fillRect(
+                this.x-(btnPadW/2), 
+                this.y-btnYOffset, 
+                this.width+btnPadW, 
+                btnHeight+btnPadH
+            );
+            context.strokeRect(
+                this.x-(btnPadW/2), 
+                this.y-btnYOffset, 
+                this.width+btnPadW, 
+                btnHeight+btnPadH
+            )
+            context.fillStyle = color4;
+            context.fillText('ok', this.x, this.y);
             this.width = context.measureText(this.text).width;
         }
 
